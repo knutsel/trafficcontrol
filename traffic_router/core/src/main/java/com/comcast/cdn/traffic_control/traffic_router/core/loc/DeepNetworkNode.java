8d8701f159990ac16f0cb77220188ddc11eca399
Jan van Doorn <jan_vandoorn@cable.comcast.com>
2016-10-11 16:32:19 +0000

    add DeepNetworkNode

---
+++ traffic_router/core/src/main/java/com/comcast/cdn/traffic_control/traffic_router/core/loc/DeepNetworkNode.java
@@ -0,0 +1,7 @@
+package com.comcast.cdn.traffic_control.traffic_router.core.loc;
+
+public class DeepNetworkNode extends NetworkNode {
+    public DeepNetworkNode(final String str) throws NetworkNodeException {
+        super(str, null);
+    }
+}
