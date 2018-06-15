1b66b6a727bbb28c68113f8cd222e4926976ecd8
Jan van Doorn <jvd@knutsel.com>
2018-06-15 12:16:31 -0600

    remove static

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,8 +110,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'sphinx_rtd_theme'
-#html_theme_path = ["_themes", ]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -141,7 +141,7 @@ html_favicon = './favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 
 
